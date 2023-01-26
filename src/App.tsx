0b864590420a35390b2e61d5a42181d3a21aec62
AkejuTaiwo <AkejuTaiwo2019@gmail.com>
2023-01-27 00:53:58 +0100

    HomePage
    Configure the Homepage of project

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
 import Navbar from '@/scenes/navbar';
+import Home from '@/scenes/home';
 import { useState } from 'react';
-function App() {
+import { SelectedPage } from '@/shared/types';
+import { useEffect } from 'react';
 
-  const [selectedPage, setSelectedPage] = useState("home")
+SelectedPage
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if( window.scrollY !== 0 ) setIsTopOfPage(false); 
+    }
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, []);
+
   return (
     <div className="app bg-gray-20">
        <Navbar 
-          selectedPage={selectedPage}
+          isTopOfPage={isTopOfPage}
+          selectedPage={selectedPage} 
           setSelectedPage={setSelectedPage}
        />
+       <Home setSelectedPage={setSelectedPage} />
     </div>
   )
 }
